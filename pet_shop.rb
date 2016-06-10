765f21138c934c5446f9b9ef37305810851949c5
Josh Kearns <jmdkearns@gmail.com>
2016-06-10 14:29:49 +0100

    9 function tests passed.

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,30 +2,42 @@ def pet_shop_name(info)
   info[:name]
 end
 
+
 def total_cash(cash)
   cash[:admin][:total_cash]
 end
+
 
 def add_or_remove_cash(cash, cash_change)
   cash[:admin][:total_cash] += cash_change
 end
 
+
 def pets_sold(sold)
   sold[:admin][:pets_sold]
 end
+
 
 def increase_pets_sold(sold, sold_increase)
   sold[:admin][:pets_sold] += sold_increase
 end
 
+
 def stock_count(stock)
   stock[:pets].length
 end
 
-def pets_by_breed(pet_shop, breed)
+
+def pets_by_breed(pet_shop, expected_breed)
   breed_array = []
 
   for pet in pet_shop[:pets]
-    breed_array << pet if pet_shop[:pets] ==  breed
+    breed_array << pet if pet[:breed] ==  expected_breed
+  end
+  return breed_array
 end
-end
+
+
+
+
+
