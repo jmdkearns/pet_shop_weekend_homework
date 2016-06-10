0576ceac3b640fcac560048fee68756f554e205c
Josh Kearns <jmdkearns@gmail.com>
2016-06-10 14:39:45 +0100

    11 function tests passed.

--- pet_shop.rb
+++ pet_shop.rb
@@ -38,6 +38,13 @@ def pets_by_breed(pet_shop, expected_breed)
 end
 
 
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    return pet if pet[:name] == name
+  end
+  return nil
+end
+
 
 
 
